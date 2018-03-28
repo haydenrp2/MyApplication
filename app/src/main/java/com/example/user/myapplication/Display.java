fbc0a8eb945603277d44a983db80d3a9694537a7
haydenrp2 <haydenrp2@mymail.vcu.edu>
2018-03-27 23:13:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/user/myapplication/Display.java
@@ -0,0 +1,16 @@
+package com.example.user.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by User on 3/27/2018.
+ */
+
+public class Display extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+    }
+}
---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.user.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v){
+        if(v.getId() == R.id.Bdisplay){
+            Intent i = new Intent(MainActivity.this, Display.class);
+            startActivity(i);
+        }
+    }
+}
